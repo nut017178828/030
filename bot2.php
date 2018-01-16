3601eff64df3dec269c4a25b93e8d88ec796a1d2
nut017178828 <35196422+nut017178828@users.noreply.github.com>
2018-01-17 02:12:34 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -32,7 +32,7 @@ else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
   }
-else if($arrJson['events'][0]['message']['text'] == "ณัฐล่อไหม?")
+else if($arrJson['events'][0]['message']['text'] == "ณัฐล่อไหม")
   {
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
