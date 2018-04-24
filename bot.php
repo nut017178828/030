d120e5e87bfd63416f9717db399b5b433b845f22
nut017178828 <35196422+nut017178828@users.noreply.github.com>
2018-04-25 04:17:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,16 +19,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => 'led on1'
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'type' => 'text',
-				'text' => 'เปิดแล้วครับ',
 				'replyToken' => $replyToken,
-				'messages' => "เปิดไฟที่1"
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
